cc6d6abe741dffca838c413f5a80028010d6332b
Ingo Karkat <swdev@ingo-karkat.de>
2021-01-08 11:45:43 +0100

    ENH: Check for and offer to enable deb-src for build-dep items
    On a vanilla Ubuntu 20.04, I get this error on apt-get build-dep:
    > E: You must put some 'deb-src' URIs in your sources.list
    The corresponding entries are there in /etc/apt/sources.list, but they are disabled by default. Detect this and offer to enable them.

---
+++ lib/enableDebSrc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+readonly scriptDir="$([ "${BASH_SOURCE[0]}" ] && dirname -- "${BASH_SOURCE[0]}" || exit 3)"
+[ -d "$scriptDir" ] || { echo >&2 "ERROR: Cannot determine script directory!"; exit 3; }
+
+readonly APT_SOURCES="${1:?}"
+
+"${scriptDir}/../../../../lib/withUnixhome" writeOrigOrBackup "$APT_SOURCES"
+sed -i -e 's/^# deb-src/deb-src/' -- "$APT_SOURCES"
+apt update
