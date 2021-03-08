30a6e46fa55ee847e6bcd73dc4e97c799ca45010
Ingo Karkat <swdev@ingo-karkat.de>
2021-03-08 20:31:47 +0100

    Refactoring: Extract require: implementation into lib/types/require.sh and use a separate definitionFilterTypeRegistry for those
    Using the same configUsage<Typename>() function for its documentation (which is always printed after the types and before preinstall:, in arbitrary order).
    A isDefinitionAcceptedBy<Typename>() function acts as the predicate for the whole definition.

---
+++ lib/types/require.sh
@@ -0,0 +1,59 @@
+#!/bin/bash source-this-script
+
+configUsageRequire()
+{
+    cat <<'HELPTEXT'
+require: items consist of a CHECK that can be:
+- another ITEM (in abbreviated form, usually just having one "package name"
+  parameter); for packages installed via the distribution's package manager, use
+  the special "native:" prefix here. The check passes if that ITEM has already
+  been installed or has already been selected by the user in the current
+  session.
+- an executable command (potentially followed by command-line arguments) in the
+  ./etc/require directory tree that is invoked and should fail if the
+  requirements are not fulfilled.
+- a REQUIREMENT-EXPRESSION (whitespace must be escaped or the entire expression
+  quoted!) that is eval'd and should fail if the requirements are not fulfilled.
+HELPTEXT
+}
+
+isDefinitionAcceptedByRequire()
+{
+    local requirement="${1:?}"; shift
+    local definition="${1:?}"; shift
+
+    local name="${requirement#*:}"
+    local prefix="${requirement%"$name"}"
+    local typeFunction=; [ -n "$prefix" ] && typeFunction="${typeRegistry["$prefix"]}"
+    if [ -n "$typeFunction" ]; then
+	local availabilityFunctionName="isAvailable${typeFunction}"
+	if type -t "$availabilityFunctionName" >/dev/null; then
+	    if ! eval "$availabilityFunctionName \"\$name\""; then
+		[ "$isVerbose" ] && printf >&2 'Skipping because requirement %s is not passed: %s\n' "$requirement" "$definition"
+		continue 2
+	    fi
+	else
+	    printf >&2 'ERROR: Type %s cannot be used for requirements checking.\n' "$prefix"
+	    exit 3
+	fi
+    elif [[ "$prefix" =~ ^[^[:space:]]+$ ]]; then
+	printf >&2 'ERROR: Invalid type: %s\n' "$prefix"
+	exit 3
+    else
+	local requirementWithoutArgs="${requirement%% *}"
+	if ! if [ -x "${requireActionsDirspec}/${requirement}" ]; then
+	    "${requireActionsDirspec}/${requirement}"
+	elif [ -x "${requireActionsDirspec}/${requirementWithoutArgs}" ]; then
+	    requirementArgs="${requirement#"${requirementWithoutArgs}"}"
+	    "${requireActionsDirspec}/${requirementWithoutArgs}" $requirementArgs
+	else
+	    eval "$requirement"
+	fi; then
+	    [ "$isVerbose" ] && printf >&2 'Skipping because requirement %s is not passed: %s\n' "$requirement" "$definition"
+	    return 1
+	fi
+    fi
+    return 0
+}
+
+definitionFilterTypeRegistry+=([require:]=Require)
