7084fc8030a22e8890c674e3accd7cbf383404be
Ingo Karkat <swdev@ingo-karkat.de>
2021-05-13 20:02:44 +0200

    tryout: ENH: Add group-tryout: type to definitionGroupFilterTypeRegistry

--- lib/types/tryout.sh
+++ lib/types/tryout.sh
@@ -8,9 +8,21 @@ definition (both preceding and following items) will only be considered if the
 environment variable TRYOUT is set.
 HELPTEXT
 }
+configUsageGroupTryout()
+{
+    cat <<'HELPTEXT'
+group-tryout: stands on its own (nothing is directly following it); all lines
+following it in the definition group will only be considered if the environment
+variable TRYOUT is set.
+HELPTEXT
+}
 
 if [ -n "$TRYOUT" ]; then
     isDefinitionAcceptedByTryout()
+    {
+	return 0
+    }
+    isDefinitionGroupAcceptedByGroupTryout()
     {
 	return 0
     }
@@ -23,6 +35,15 @@ else
 	[ "$isVerbose" ] && printf >&2 'Skipping tryout definition because TRYOUT is not set: %s\n' "$definition"
 	return 1
     }
+    isDefinitionGroupAcceptedByGroupTryout()
+    {
+	shift # group filter
+	local definitionGroupFileAndLocation="${1:?}"; shift
+
+	[ "$isVerbose" ] && printf >&2 'Skipping tryout definition group filter for %s because TRYOUT is not set\n' "$definitionGroupFileAndLocation"
+	return 1
+    }
 fi
 
 definitionFilterTypeRegistry+=([tryout:]=Tryout)
+definitionGroupFilterTypeRegistry+=([group-tryout:]=GroupTryout)
