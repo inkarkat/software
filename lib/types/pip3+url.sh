4a0eeacbc971c1162a69bcd5f5c836559cc2f503
Ingo Karkat <swdev@ingo-karkat.de>
2022-02-20 13:29:40 +0100

    pip3+url: ENH: Use new pip3-download-installer to support local install dir
    The syntax of the item changes from
        PACKAGE:URL
    to
        PACKAGE[:MAX-AGE[SUFFIX]]:[[SUBDIR/]NAME/]PACKAGE-GLOB:[URL [...]]

--- lib/types/pip3+url.sh
+++ lib/types/pip3+url.sh
@@ -4,46 +4,69 @@ configUsagePip3Url()
 {
     cat <<HELPTEXT
 pip3+url: items consist of a
-    NAME:URL
-pair, where URL will be used to install the NAME package.
+    PACKAGE[:MAX-AGE[SUFFIX]]:[[SUBDIR/]NAME/]PACKAGE-GLOB:[URL [...]]
+triplet / quadruplet.
+If ${INSTALL_REPO}/(SUBDIR|*)/(NAME|*)/PACKAGE-GLOB already exists
+[and if it is younger than MAX-AGE[SUFFIX]], it will be used; else, the tar
+archive from URL(s) (first that succeeds) will be downloaded (and put into
+${INSTALL_REPO}/* if it exists).
+If no URL is given and the package does not exist, the installation will fail.
 HELPTEXT
 }
 
-typeset -A addedPip3Urls=()
+typeset -A addedPip3UrlRecords=()
 hasPip3Url()
 {
-    local packageName="${1%%:*}"
-    local pip3Url="${1#"${packageName}:"}"
-    if [ -z "$packageName" -o -z "$pip3Url" ]; then
+    if [[ ! "$1" =~ ^[^:]+:.+: ]]; then
 	printf >&2 'ERROR: Invalid pip3+url item: "pip3+url:%s"\n' "$1"
 	exit 3
     fi
 
-    [ "${addedPip3Urls["$packageName"]}" ] && return 0	# This repo has already been selected for installation.
-
-    hasPip3 "$packageName"
+    hasPip3 "${1%%:*}" || [ "${addedPip3UrlRecords["$1"]}" ]
 }
 
 addPip3Url()
 {
-    local pip3KeyRecord="${1:?}"; shift
-    local pip3PackageName="${pip3KeyRecord%%:*}"
-    local pip3Url="${pip3KeyRecord#"${pip3PackageName}:"}"
+    local pip3UrlRecord="${1:?}"; shift
+    local pip3PackageName="${pip3UrlRecord%%:*}"
 
     isAvailableOrUserAcceptsNative pip3 python3-pip 'pip3 Python 3 package manager' || return $?
 
     preinstallHook "$pip3PackageName"
-    addedPip3Urls["$pip3PackageName"]="$pip3Url"
+    addedPip3UrlRecords["$pip3UrlRecord"]=t
     externallyAddedPip3Packages["$pip3PackageName"]=t
     postinstallHook "$pip3PackageName"
 }
 
 installPip3Url()
 {
-    [ ${#addedPip3Urls[@]} -gt 0 ] || return
+    [ ${#addedPip3UrlRecords[@]} -gt 0 ] || return
 
-    local quotedPip3Urls; printf -v quotedPip3Urls ' %q' "${addedPip3Urls[@]}"
-    submitInstallCommand "${SUDO}${SUDO:+ }pip3${isBatch:+ --yes} install$quotedPip3Urls"
+    local pip3UrlRecord; for pip3UrlRecord in "${!addedPip3UrlRecords[@]}"
+    do
+	local maxAge=
+	local applicationNamePackageGlobUrl="${pip3UrlRecord#*:}"
+	if [[ "$applicationNamePackageGlobUrl" =~ ^[0-9]+([smhdwyg]|mo): ]]; then
+	    maxAge="${BASH_REMATCH[0]%:}"
+	    applicationNamePackageGlobUrl="${applicationNamePackageGlobUrl#"${BASH_REMATCH[0]}"}"
+	fi
+	local urlList="${applicationNamePackageGlobUrl#*:}"
+	local applicationNameAndPackageGlob="${applicationNamePackageGlobUrl%:$urlList}"
+	local packageGlob="${applicationNameAndPackageGlob##*/}"
+	local applicationName="${applicationNameAndPackageGlob%"$packageGlob"}"
+	local outputNameArg=; isglob "$packageGlob" || printf -v outputNameArg %q "$packageGlob"
+	printf -v packageGlob %q "$packageGlob"
+	applicationName="${applicationName%/}"
+	printf -v applicationName %q "$applicationName"
+	typeset -a urls=(); IFS=' ' read -r -a urls <<<"$urlList"
+	local urlArgs=''; [ ${#urls[@]} -gt 0 ] && printf -v urlArgs ' --url %q' "${urls[@]}"
+
+	# Note: No sudo here, as the downloading will happen as the current user
+	# and only the installation itself will be done through sudo.
+	submitInstallCommand \
+	    "pip3-download-installer${isBatch:+ --batch}${applicationName:+ --application-name }${applicationName} --expression ${packageGlob}${maxAge:+ --max-age }$maxAge${urlArgs}${outputNameArg:+ --output }${outputNameArg}" \
+	    "${decoration["pip3+url:$pip3UrlRecord"]}"
+    done
 }
 
 typeRegistry+=([pip3+url:]=Pip3Url)
