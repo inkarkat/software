acb9848369d9688df0a67499e452344cd0930fa4
Ingo Karkat <swdev@ingo-karkat.de>
2022-07-29 17:52:39 +0200

    Add apt-reinstall variant of apt-remove
    We cannot simply use apt-remove:FOO apt:FOO, as the check for missing items runs over the entire definition, and only based on that static selection of missing packages will stuff be added. Now this could be solved by distributing the items over multiple eponymous definitions linked by a dummy item, but that's clumsy and not very readable.
    apt-reinstall works like apt-remove, but also schedules the package for addition; the typeInstallOrder then ensures that those run in the right order.

--- lib/types/apt-remove.sh
+++ lib/types/apt-remove.sh
@@ -6,14 +6,28 @@ configUsageAptRemove()
 apt-remove: items refer to Debian packages to be uninstalled via apt.
 HELPTEXT
 }
+configUsageAptReinstall()
+{
+    cat <<'HELPTEXT'
+apt-reinstall: items refer to Debian packages to be uninstalled (if installed)
+and then installed again (presumably after reconfiguring the package sources)
+via apt. Behaves like a normal installation if the package hasn't been
+installed.
+HELPTEXT
+}
 
 typeRegistry+=([apt-remove:]=AptRemove)
-typeInstallOrder+=([10]=AptRemove)
+typeRegistry+=([apt-reinstall:]=AptReinstall)
+typeInstallOrder+=([9]=AptRemove)
+typeInstallOrder+=([10]=AptReinstall)
 
 if ! exists apt; then
     hasAptRemove() { return 98; }
+    hasAptReinstall() { return 98; }
     installAptRemove() { :; }
+    installAptReinstall() { :; }
     isAvailableAptRemove() { return 98; }
+    isAvailableAptReinstall() { return 98; }
     return
 fi
 
@@ -29,6 +43,16 @@ hasAptRemove()
 
     [ ! "${installedAptPackages["$packageName"]}" ] || [ "${removedAptPackages["$packageName"]}" ] || [ "${externallyRemovedAptPackages["$packageName"]}" ]
 }
+hasAptReinstall()
+{
+    local packageName="${1:?}"; shift
+    if ! getInstalledAptPackages; then
+	messagePrintf >&2 'ERROR: Failed to obtain installed native package list; skipping %s.\n' "$packageName"
+	return 99
+    fi
+
+    [ "${removedAptPackages["$packageName"]}" ] || [ "${externallyRemovedAptPackages["$packageName"]}" ]
+}
 
 addAptRemove()
 {
@@ -36,10 +60,20 @@ addAptRemove()
     removedAptPackages["$packageName"]=t
     unset "installedAptPackages[$packageName]"	# The package won't be available any longer.
 }
+addAptReinstall()
+{
+    local packageName="${1:?}"; shift
+    removedAptPackages["$packageName"]=t
+    addedAptPackages["$packageName"]=t
+}
 
 isAvailableAptRemove()
 {
     isQuiet=t hasAptRemove "$@"
+}
+isAvailableAptReinstall()
+{
+    isQuiet=t hasAptReinstall "$@"
 }
 
 installAptRemove()
@@ -47,4 +81,9 @@ installAptRemove()
     [ ${#removedAptPackages[@]} -gt 0 ] || return
     local IFS=' '
     submitInstallCommand "${SUDO}${SUDO:+ }apt${isBatch:+ --assume-yes} remove ${!removedAptPackages[*]}"
+}
+installAptReinstall()
+{
+    # Noop; the actual actions are done by installAptRemove() and installApt().
+    :
 }
