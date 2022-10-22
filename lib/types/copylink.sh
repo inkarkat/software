da8f3f21ad22acc7df317e4cb2cc1d6cd9869d5e
Ingo Karkat <swdev@ingo-karkat.de>
2022-10-22 19:00:31 +0200

    Add copy: variant of link:
    This is a close alternative to install:; however, the intention here is more for data files, not executables. (But admittedly install: can be made to copy data files as well; I've mostly added this because it was trivial to do so, with the addCopy command already there and almost identical in use to addSymlink.)
    Note that the functions are misspelled as "*Kopy" because the external command already is named addCopy, so it would interfere with the setup-software API. (This could be solved via "command addCopy", but that would then also leak out into the generated commands, unless I'd add a flag or something...)

--- lib/types/copylink.sh
+++ lib/types/copylink.sh
@@ -15,6 +15,22 @@ You can specify additional symlink arguments:
 A missing path to NEW-LINK is created automatically.
 HELPTEXT
 }
+configUsageKopy()
+{
+    cat <<HELPTEXT
+copy: items consist of
+    [ADDCOPY-ARGS ...] SOURCE DEST
+SOURCE is either relative to the ./etc/files directory tree, or an absolute
+filespec and is copied to DEST unless the latter already exists.
+You can specify additional symcopy arguments:
+    --sudo		Do the copying with sudo unless already running as the
+			superuser.
+    --force|-f		Overwrite (individual files in) an existing DEST.
+    --no-target-directory
+			Treat DEST as a normal file.
+A missing path to DEST is created automatically.
+HELPTEXT
+}
 
 parseLink()
 {
@@ -49,9 +65,45 @@ parseLink()
     printf '%q ' addSymlink "${commonArgs[@]}" "${linkArgs[@]}" -- "$sourceFilespec"
     printf %q "$2"
 }
+parseKopy()
+{
+    local copyItem="${1:?}"; shift
+    typeset -a commonArgs=("$@")
+    eval "set -- $copyItem"
+
+    typeset -a copyArgs=()
+    while [ $# -ne 0 ]
+    do
+	case "$1" in
+	    --sudo)	commonArgs+=("$1"); shift;;
+	    --force|-f|--no-target-directory)
+			copyArgs+=("$1"); shift;;
+
+	    --)		shift; break;;
+	    -*)		printf >&2 'ERROR: Invalid copy item: "copy:%s" due to invalid "%s".\n' "$copyItem" "$1"; exit 3;;
+	    *)		break;;
+	esac
+    done
+    if [ $# -ne 2 ]; then
+	printf >&2 'ERROR: Invalid copy item: "copy:%s" due to missing SOURCE DEST.\n' "$copyItem"
+	exit 3
+    fi
+
+    local sourceFilespec; if ! sourceFilespec="$(getAbsoluteOrFilesFilespec "$1")"; then
+	printf >&2 'ERROR: Invalid copy item: "copy:%s" due to missing SOURCE: "%s".\n' "$copyItem" "$1"
+	exit 3
+    fi
+
+    printf '%q ' addDir "${commonArgs[@]}" --
+    printf '%q && ' "$(dirname -- "$2")"
+    printf '%q ' addCopy "${commonArgs[@]}" "${copyArgs[@]}" -- "$sourceFilespec"
+    printf %q "$2"
+}
 
 typeset -A addedLinkActions=()
 typeset -a addedLinkActionList=()
+typeset -A addedKopyActions=()
+typeset -a addedKopyActionList=()
 hasLink()
 {
     local linkRecord="${1:?}"
@@ -61,6 +113,17 @@ hasLink()
     [ "${addedLinkActions["$quotedLinkCommand"]}" ] && return 0	# This link action has already been selected for linking.
 
     local decoration="${decoration["link:$linkRecord"]}"
+    eval "$(decorateCommand "$quotedCheckCommand" "$decoration")"
+}
+hasKopy()
+{
+    local copyRecord="${1:?}"
+    local quotedCheckCommand; quotedCheckCommand="$(parseKopy "$copyRecord" --check)" || exit 3
+    local quotedKopyCommand; quotedKopyCommand="$(parseKopy "$copyRecord" --accept-up-to-date)" || exit 3
+
+    [ "${addedKopyActions["$quotedKopyCommand"]}" ] && return 0	# This copy action has already been selected for copying.
+
+    local decoration="${decoration["copy:$copyRecord"]}"
     eval "$(decorateCommand "$quotedCheckCommand" "$decoration")"
 }
 
@@ -74,6 +137,16 @@ addLink()
     addedLinkActions["$quotedLinkCommand"]="$linkRecord"
     addedLinkActionList+=("$quotedLinkCommand")
 }
+addKopy()
+{
+    local copyRecord="${1:?}"
+    local quotedKopyCommand; quotedKopyCommand="$(parseKopy "$copyRecord" --accept-up-to-date)" || exit 3
+    # Note: Do not support pre-/postinstall hooks here (yet), as there's no good
+    # short "name" that we could use. The DEST's whole path may be a bit
+    # long, and just the filename itself may be ambiguous.
+    addedKopyActions["$quotedKopyCommand"]="$copyRecord"
+    addedKopyActionList+=("$quotedKopyCommand")
+}
 installLink()
 {
     [ ${#addedLinkActions[@]} -eq ${#addedLinkActionList[@]} ] || { echo >&2 'ASSERT: Link actions dict and list sizes disagree.'; exit 3; }
@@ -84,6 +157,18 @@ installLink()
 	submitInstallCommand "$quotedLinkCommand" "${decoration["link:${addedLinkActions["$quotedLinkCommand"]}"]}"
     done
 }
+installKopy()
+{
+    [ ${#addedKopyActions[@]} -eq ${#addedKopyActionList[@]} ] || { echo >&2 'ASSERT: Kopy actions dict and list sizes disagree.'; exit 3; }
+    [ ${#addedKopyActionList[@]} -gt 0 ] || return
+
+    local quotedKopyCommand; for quotedKopyCommand in "${addedKopyActionList[@]}"
+    do
+	submitInstallCommand "$quotedKopyCommand" "${decoration["copy:${addedKopyActions["$quotedKopyCommand"]}"]}"
+    done
+}
 
 typeRegistry+=([link:]=Link)
 typeInstallOrder+=([882]=Link)
+typeRegistry+=([copy:]=Kopy)
+typeInstallOrder+=([883]=Kopy)
