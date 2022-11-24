9fcd156f96c3e55477644c52e04e27c64757701d
Ingo Karkat <swdev@ingo-karkat.de>
2022-11-24 10:02:43 +0100

    Compatibility: IrfanView cannot be installed on Ubuntu 22.04
    On hyrican1, LingoPad installs fine, but not IrfanView:
    > 0024:err:module:process_init L"Z:\\media\\inkarkat\\Kn\00f6pfle\\install\\applications\\IrfanView\\iview460_x64_setup.exe" 64-bit application not supported in 32-bit prefix
    
    Seems like I cannot create a 64-bit context:
    > $ rm -rf ~/.wine
    > $ WINEARCH=win64 winecfg
    > wine: created the configuration directory '/home/inkarkat/.wine'
    > wine: WINEARCH set to win64 but '/home/inkarkat/.wine' is a 32-bit installation.
    
    I wonder what's different on Ubuntu 22.04 vs. 20.04, but explicitly installing wine64 helps. Source: https://askubuntu.com/a/1194312/72217

--- lib/types/wine+url.sh
+++ lib/types/wine+url.sh
@@ -56,6 +56,7 @@ addWine()
     local wineUrlRecord="${1:?}"; shift
     isAvailableOrUserAcceptsNative wine || return $?
     isAvailableOrUserAcceptsNative wine32 || return $?
+    isAvailableOrUserAcceptsNative wine64 || return $?
 
     # The best identifier for pre-/postinstall hooks is the package name
     # (without any [SUBDIR/]); unfortunately, it is optional and hard to parse,
