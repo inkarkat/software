aec8c0b2bab7a031c14163c30731e2dd43a23e64
Ingo Karkat <swdev@ingo-karkat.de>
2023-03-27 14:47:41 +0200

    CHG: ENH: Add mandatory RESULT-FILE and also trigger the build if that does not exist (but the working copy is up-to-date)
    Else, we'd have to manually go into the repository and retrigger the build steps if the original build somehow failed.

--- lib/types/gitrepo.sh
+++ lib/types/gitrepo.sh
@@ -6,21 +6,22 @@ configUsageGitrepo()
 {
     cat <<'HELPTEXT'
 gitrepo: items consist of a
-    NAME[:MAX-AGE[SUFFIX]]:GIT-URL[:(BRANCH|TAG|TAG-GLOB)]:BUILD-COMMAND
-triplet / quadruplet / quintuplet.
+    NAME[:MAX-AGE[SUFFIX]]:GIT-URL[:(BRANCH|TAG|TAG-GLOB)]:RESULT-FILE:BUILD-COMMAND
+quadruplet / quintuplet / sextuplet.
 If no Git working copy exists yet at ${GITREPO_BASEDIR}/NAME (NAME can also be
 an absolute path), the Git repository at GIT-URL is cloned there [and BRANCH (or
 a TAG, or the highest version matching TAG-GLOB) checked out].
 If the last pull date is older than MAX-AGE[SUFFIX], the remote will be checked
 for changes on the branch / a newer tag, and if such exist, these will be
-checked out and BUILD-COMMAND executed.
+checked out. BUILD-COMMAND is executed unless there have been no updates and
+RESULT-FILE (absolute or relative to the working copy root) already exists.
 HELPTEXT
 }
 
 parseGitrepo()
 {
     local gitrepoRecord="${1:?}"; shift
-    if [[ ! "$gitrepoRecord" =~ ^[^:[:space:]]+:[^:[:space:]]+:.+ ]]; then
+    if [[ ! "$gitrepoRecord" =~ ^[^:[:space:]]+:[^:[:space:]]+:[^:]+:.+ ]]; then
 	printf >&2 'ERROR: Invalid gitrepo item: "gitrepo:%s"\n' "$gitrepoRecord"
 	return 3
     fi
@@ -37,14 +38,15 @@ parseGitrepo()
 	maxAge="${BASH_REMATCH[0]%:}"
 	remainder="${remainder#"${BASH_REMATCH[0]}"}"
     fi
-    if [[ ! "$remainder" =~ ^((/|[^:[:space:]]+://|[^:[:space:]]+@[^:[:space:]]+:)[^:[:space:]]+)(:([^:[:space:]]+))?:(.+)$ ]]; then
+    if [[ ! "$remainder" =~ ^((/|[^:[:space:]]+://|[^:[:space:]]+@[^:[:space:]]+:)[^:[:space:]]+)(:([^:[:space:]]+))?:([^:[:space:]]+):(.+)$ ]]; then
 	printf >&2 'ERROR: Invalid gitrepo item: "gitrepo:%s"\n' "$gitrepoRecord"
 	return 3
     fi
     local gitUrl="${BASH_REMATCH[1]}"
     local branch="${BASH_REMATCH[4]}"
-    local buildCommand="${BASH_REMATCH[5]}"
-    printf 'local %s=%q\n' location "$location" maxAge "$maxAge" gitUrl "$gitUrl" branch "$branch" buildCommand "$buildCommand"
+    local resultFile="${BASH_REMATCH[5]}"
+    local buildCommand="${BASH_REMATCH[6]}"
+    printf 'local %s=%q\n' location "$location" maxAge "$maxAge" gitUrl "$gitUrl" branch "$branch" resultFile "$resultFile" buildCommand "$buildCommand"
 }
 
 typeset -A addedGitrepoLocations=()
@@ -59,9 +61,15 @@ hasGitrepo()
     exists git || return 1
     exists git-iscontrolled || return 1
     git-iscontrolled "$location" || return 1
-    [ -n "${maxAge?}" ] || return 0
-    git-inside fetchdate --remote upstream --older "$maxAge" -- "$location" || return 0
-    git-inside uptodate --quiet upstream -- "$location"
+
+    if [ -n "${maxAge?}" ] \
+	&& git-inside fetchdate --remote upstream --older "$maxAge" -- "$location"
+    then
+	git-inside uptodate --quiet upstream -- "$location" || return 1
+    fi
+
+    local quotedResultFile; printf -v quotedResultFile '%q' "$resultFile"
+    git-inside --command "test -e $quotedResultFile" -- "$location"
 }
 
 addGitrepo()
@@ -90,7 +98,7 @@ installGitrepo()
 	local parse; parse="$(parseGitrepo "$gitrepoRecord")" || exit 3; eval "$parse"
 	local quotedGitrepoInstallCommand; printf -v quotedGitrepoInstallCommand '%q ' \
 	    "${projectDir}/lib/gitrepoInstall.sh" \
-	    "${location:?}" "${gitUrl:?}" "${branch?}" "${buildCommand:?}"
+	    "${location:?}" "${gitUrl:?}" "${branch?}" "${resultFile:?}" "${buildCommand:?}"
 	submitInstallCommand "${quotedGitrepoInstallCommand% }" "${decoration["gitrepo:${gitrepoRecord}"]}"
     done
 }
