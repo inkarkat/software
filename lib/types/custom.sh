03d97178e39ecf1a9640cf429c59870ea3c1e5e4
Ingo Karkat <swdev@ingo-karkat.de>
2023-12-08 21:03:32 +0100

    Generalization: Use metaprogramming to be able to support multiple custom action variants

--- lib/types/custom.sh
+++ lib/types/custom.sh
@@ -1,19 +1,21 @@
 #!/bin/bash source-this-script
 
-configUsageCustom()
+configUsageCustomAtOrder()
 {
-    cat <<'HELPTEXT'
-custom: items consist of a CHECK:ACTION pair, where the latter will be chosen if
+    local order="${1?}"; shift
+    local when="${1:?}"; shift
+    cat <<HELPTEXT
+custom${order}: items consist of a CHECK:ACTION pair, where the latter will be chosen if
 the former does not succeed.
 CHECK can be one of the following (in decreasing precedence):
 - an EXECUTABLE-COMMAND in the ./etc/custom directory tree that is called and
   should succeed if the application already exists, and fail if it is missing;
   if EXECUTABLE-COMMAND starts with a &, this is replaced by the following
-  ACTION (without a $SUDO prefix), allowing you to save repeated typing:
-	custom:&-check:foo-installer
-  (Prepend $SUDO if the command needs to be invoked as root; but try your best
+  ACTION (without a \$SUDO prefix), allowing you to save repeated typing:
+	custom${order}:&-check:foo-installer
+  (Prepend \$SUDO if the command needs to be invoked as root; but try your best
   to avoid that.)
-- an EXECUTABLE-NAME? (located through $PATH) or GLOB? (potentially prefixed
+- an EXECUTABLE-NAME? (located through \$PATH) or GLOB? (potentially prefixed
   with !), and succeeds if it's (with !: not) there / resolves to an existing
   file, directory, or symlink
 - the special expression "once"; then, no check is performed and the
@@ -22,17 +24,17 @@ CHECK can be one of the following (in decreasing precedence):
   quoted!) that is eval'd and should succeed if the application already
   exists, and fail if it is missing, fail with 98 if this item and with 99 if
   the entire definition should be skipped; if TEST-EXPRESSION starts with a &,
-  this is replaced by the following ACTION (without a $SUDO prefix and without
+  this is replaced by the following ACTION (without a \$SUDO prefix and without
   its command-line arguments); !* is replaced with any arguments given to
   ACTION, allowing you to re-use the same script for checking and installing:
-	custom:'& --check !* --quiet':foo-installer --recursive
+	custom${order}:'& --check !* --quiet':foo-installer --recursive
   Note: This cannot contain literal colons, as these would prematurely end the
-  TEST-EXPRESSION; you can use $(echo -e \\x3a) instead of : as a workaround.
-  (Prepend $SUDO if the expression needs to be invoked as root; but try your
+  TEST-EXPRESSION; you can use \$(echo -e \\\\x3a) instead of : as a workaround.
+  (Prepend \$SUDO if the expression needs to be invoked as root; but try your
   best to avoid that.)
-ACTION is one of the following:
+ACTION $when and is one of the following:
 - an EXECUTABLE-COMMAND (potentially followed by command-line arguments) in the
-  ./etc/custom directory tree that is invoked (prepend $SUDO if it needs to be
+  ./etc/custom directory tree that is invoked (prepend \$SUDO if it needs to be
   invoked as root) and should then install the application
 - a TEXT-FILE in the ./etc/custom directory tree whose file name (without
   extension) is taken as a notification title and contents as notification to be
@@ -41,11 +43,15 @@ ACTION is one of the following:
 - another ITEM (that is then executed as usual); for packages installed via the
   distribution's package manager, use the special "native:" prefix here
 - an INSTALL-EXPRESSION (whitespace must be escaped or the entire expression
-  quoted!) that is eval'd (prepend $SUDO if it needs to be invoked as root)
+  quoted!) that is eval'd (prepend \$SUDO if it needs to be invoked as root)
 Environment variables for the check and custom action can be passed via
     config:VAR=NAME
 configuration item(s) preceding it.
 HELPTEXT
+}
+configUsageCustom()
+{
+    configUsageCustomAtOrder '' 'is executed at the very end of the installation' "$@"
 }
 
 customPathOrGlobCheck()
@@ -80,8 +86,9 @@ getQuotedCustomOnceMarkerCommand()
 typeset -A addedCustomActions=()
 typeset -a addedCustomActionList=()
 typeset -A addedCustomConfigs=()
-hasCustom()
+hasCustomAtOrder()
 {
+    local order="${1?}"; shift
     local customRecord="${1:?}"; shift
     local customAction="${customRecord#*:}"
     local customCheck="${customRecord%":$customAction"}"
@@ -90,15 +97,15 @@ hasCustom()
     local customActionWithoutSudoAndArgs="${customAction#\$SUDO }"; customActionWithoutSudoAndArgs="${customActionWithoutSudoAndArgs%% *}"
 
     if [ -z "$customAction" -o -z "$customCheck" ]; then
-	printf >&2 'ERROR: Invalid custom item: "custom:%s"\n' "$1"
+	printf >&2 'ERROR: Invalid custom item: "custom%s:%s"\n' "$order" "$1"
 	exit 3
     fi
 
-    [ "${addedCustomActions["$customAction"]}" ] && return 0	# This custom action has already been selected for installation.
+    eval "[ \"\${addedCustom${order^}Actions[\"\$customAction\"]}\" ]" && return 0	# This custom action has already been selected for installation.
 
-    local config="${hasConfiguration["custom:$customRecord"]}"; config="${config//$'\n'/ }"
+    local config="${hasConfiguration["custom${order}:${customRecord}"]}"; config="${config//$'\n'/ }"
     local customFilespec customCheckCommand
-    local customDecoration="${decoration["custom:${customRecord}"]}"
+    local customDecoration="${decoration["custom${order}:${customRecord}"]}"
     if [ "$customCheck" = 'once' ]; then
 	eval "${config}${config:+ }$(getQuotedCustomOnceMarkerCommand --query "$customAction")"
     elif customFilespec="$(getCustomFilespec -x "${customCheckWithoutSudo}")"; then
@@ -129,22 +136,27 @@ hasCustom()
 	invokeCheck "$(decorateCommand "${config}${config:+ }${customCheckCommand}" "$customDecoration")"
     fi
 }
+hasCustom()
+{
+    hasCustomAtOrder '' "$@"
+}
 
 typeset -A itemCustomActions=()
 typeset -A onceCustomActions=()
-addCustom()
+addCustomAtOrder()
 {
+    local order="${1?}"; shift
     # Note: Do not support pre-/postinstall hooks here, as we have no short
     # "name" that we could use.
     local customRecord="${1:?}"; shift
     local customAction="${customRecord#*:}"
     local customCheck="${customRecord%":$customAction"}"
-    addedCustomActions["$customAction"]="$customRecord"
-    addedCustomActionList+=("$customAction")
-    addedCustomConfigs["$customAction"]="${configuration["custom:$customRecord"]}"
+    eval "addedCustom${order^}Actions[\"\$customAction\"]=\"\$customRecord\""
+    eval "addedCustom${order^}ActionList+=(\"\$customAction\")"
+    eval "addedCustom${order^}Configs[\"\$customAction\"]=\"\${configuration[\"custom${order}:\${customRecord}\"]}\""
 
     if [ "$customCheck" = 'once' ]; then
-	onceCustomActions["$customAction"]=t
+	eval "onceCustom${order^}Actions[\"\$customAction\"]=t"
     fi
 
     local customActionWithoutSudo="${customAction#\$SUDO }"
@@ -158,7 +170,7 @@ addCustom()
 	if [ -n "$prefix" ]; then
 	    local typeFunction="${typeRegistry["${prefix}"]}"
 	    if [ -n "$typeFunction" ]; then
-		itemCustomActions["$customAction"]=t
+		eval "itemCustom${order^}Actions[\"\$customAction\"]=t"
 		"add${typeFunction}" "$name"
 
 		if [ "$customCheck" = 'once' ]; then
@@ -170,24 +182,33 @@ addCustom()
 	fi
     fi
 }
-
-installCustom()
+addCustom()
 {
-    [ ${#addedCustomActions[@]} -eq ${#addedCustomActionList[@]} ] || { echo >&2 'ASSERT: Custom actions dict and list sizes disagree.'; exit 3; }
-    [ ${#addedCustomActionList[@]} -gt 0 ] || return
+    addCustomAtOrder '' "$@"
+}
 
-    local customAction; for customAction in "${addedCustomActionList[@]}"
+installCustomAtOrder()
+{
+    local order="${1?}"; shift
+    eval "[ \${#addedCustom${order^}Actions[@]} -eq \${#addedCustom${order^}ActionList[@]} ]" || { echo >&2 'ASSERT: Custom actions dict and list sizes disagree.'; exit 3; }
+    eval "[ \${#addedCustom${order^}ActionList[@]} -gt 0 ]" || return
+
+    local -n actionList=addedCustom${order^}ActionList
+    local -n configs=addedCustom${order^}Configs
+    local -n actions=addedCustom${order^}Actions
+    local customAction; for customAction in "${actionList[@]}"
     do
-	local config="${addedCustomConfigs["$customAction"]}"; config="${config//$'\n'/ }"
+	local config="${configs["$customAction"]}"; config="${config//$'\n'/ }"
 	local customActionWithoutSudo="${customAction#\$SUDO }"
 	local customActionWithoutSudoAndArgs="${customActionWithoutSudo%% *}"
 	local sudoPrefix="${customAction%"$customActionWithoutSudo"}"
 	local customFilespec
-	local customRecord="${addedCustomActions["$customAction"]}"
-	local customDecoration="${decoration["custom:$customRecord"]}"
-	local quotedCustomOnceMarkerCommand=; [ "${onceCustomActions["$customAction"]}" ] && quotedCustomOnceMarkerCommand="$(getQuotedCustomOnceMarkerCommand --update "$customAction")"
+	local customRecord="${actions["$customAction"]}"
+	local customDecoration="${decoration["custom${order}:${customRecord}"]}"
+	local quotedCustomOnceMarkerCommand=; eval "[ \"\${onceCustom${order^}Actions[\"\$customAction\"]}\" ]" && quotedCustomOnceMarkerCommand="$(getQuotedCustomOnceMarkerCommand --update "$customAction")"
 
-	if [ "${itemCustomActions["$customAction"]}" ]; then
+	eval "local hasItemCustomAction=\${itemCustom${order^}Actions[\"\$customAction\"]}"
+	if [ "$hasItemCustomAction" ]; then
 	    # The corresponding action item has already been added to the item's
 	    # type; do nothing here.
 	    continue
@@ -201,6 +222,10 @@ installCustom()
 	fi
 	submitInstallCommand "${sudoPrefix:+${SUDO}${config:+ --preserve-env}${SUDO:+ }}${config}${config:+ }${customActionWithoutSudo}${quotedCustomOnceMarkerCommand:+ && }${quotedCustomOnceMarkerCommand}" "$customDecoration"
     done
+}
+installCustom()
+{
+    installCustomAtOrder '' "$@"
 }
 
 typeRegistry+=([custom:]=Custom)
