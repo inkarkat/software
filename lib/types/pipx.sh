7d34368cc688ef2ebfa4952c40fde7dc2f39b914
Ingo Karkat <swdev@ingo-karkat.de>
2024-09-10 16:32:47 +0200

    ENH: Add pipx variant of pip3
    In Ubuntu 24.04, pip3 is more insistent that adding packages to the global Python path is wrong.
    > This environment is externally managed
    I've implemented several workarounds to differentiate between natively packaged and Python-installed packages, but this led me to a renewed search for alternative solutions, and this (now) exists in the form of pipx.
    - https://stackoverflow.com/a/78652149/813602
    - https://python.land/virtual-environments/pipx
    
    pipx installs Python packages that include a CLI (like mypy, HTTPie, or yt-dlp) in segregated environments (since version 1.5.0 globally under /opt/pipx) and adds the launcher script to /usr/local/bin/.
    That's exactly what I need.
    The logic works just like with pip3; however, there are slight differences in how optional modules are specified (as part of the package, not separately) and installed (all together, not accummulatingly, so we have to merge existing with added modules ourselves).
    
    WIP: Still missing: pipx+url type, and the ability to "pipx inject" additional dependencies

---
+++ lib/types/pipx.sh
@@ -0,0 +1,110 @@
+#!/bin/bash source-this-script
+
+configUsagePipx()
+{
+    cat <<'HELPTEXT'
+pipx: items refer to Python CLI apps installed in isolated environments.
+HELPTEXT
+}
+
+parsePipxPackageName()
+{
+    local packageSpec="${1:?}"; shift
+    printf %s "${packageSpec%%\[*\]}"
+}
+parsePipxPackageModules()
+{
+    local packageSpec="${1:?}"; shift
+    local packageName="$(parsePipxPackageName "$packageSpec")"
+    local packageModules="${packageSpec#$packageName}"
+    packageModules="${packageModules#\[}"
+    packageModules="${packageModules%\]}"
+    printf %s "$packageModules"
+}
+
+typeset -A installedPipxPackages=()
+isInstalledPipxPackagesAvailable=
+getInstalledPipxPackages()
+{
+    [ "$isInstalledPipxPackagesAvailable" ] && return
+    if ! exists pipx; then
+	# A missing pipx means that no Python CLI apps have been installed yet.
+	isInstalledPipxPackagesAvailable=t
+	return
+    fi
+
+    local exitStatus packageSpec remainder; while IFS=$'\t' read -r packageSpec remainder || { exitStatus="$packageSpec"; break; }	# Exit status from the process substitution (<(pipx-list-packages)) is lost; return the actual exit status via an incomplete (i.e. missing the newline) last line.
+    do
+	# Python CLI apps can have optional modules selected during installation:
+	# PACKAGE[MODULE1,...]. There can only be one app configuration due to the
+	# global app name.
+	local packageName="$(parsePipxPackageName "$packageSpec")"
+	local packageModules="$(parsePipxPackageModules "$packageSpec")"
+	installedPipxPackages["$packageName"]="$packageModules"
+	case ",${DEBUG:-}," in *,setup-software:pipx,*) echo >&2 "${PS4}setup-software (pipx): Found installed ${packageName}${packageModules:+ with dependencies }${packageModules}";; esac
+    done < <(pipx-list-packages --package-spec --global 2>/dev/null; printf %d "$?")
+    [ $exitStatus -eq 0 ] && isInstalledPipxPackagesAvailable=t
+}
+
+typeset -A addedPipxPackages=()
+typeset -A externallyAddedPipxPackages=()
+hasPipx()
+{
+    local packageSpec="${1:?}"; shift
+    if ! getInstalledPipxPackages; then
+	messagePrintf >&2 'ERROR: Failed to obtain installed Python CLI app list; skipping %s.\n' "$packageSpec"
+	return 99
+    fi
+
+    local packageName="$(parsePipxPackageName "$packageSpec")"
+    local packageModules="$(parsePipxPackageModules "$packageSpec")"
+    if [ -z "$packageModules" ]; then
+	# Without required modules, just the existence of the package is enough.
+	[ -n "${installedPipxPackages["$packageName"]+t}" ] || [ -n "${addedPipxPackages["$packageName"]+t}" ] || [ -n "${externallyAddedPipxPackages["$packageName"]+t}" ]
+    else
+	# Need to ensure that all required modules already are [about to be] installed.
+	test -z "$(comm -23 \
+	    <(mergeLists --field-separator , --output-separator $'\n' --sort --omit-empty -- "$packageModules") \
+	    <(mergeLists --field-separator , --output-separator $'\n' --sort --omit-empty -- "${installedPipxPackages["$packageName"]}" "${addedPipxPackages["$packageName"]}" "${externallyAddedPipxPackages["$packageName"]}"))"
+    fi
+}
+
+addPipx()
+{
+    local packageSpec="${1:?}"; shift
+    local packageName="$(parsePipxPackageName "$packageSpec")"
+    local packageModules="$(parsePipxPackageModules "$packageSpec")"
+    isAvailableOrUserAcceptsGroup pipx "${projectDir}/lib/definitions/pipx" 'pipx Python 3 package manager' || return $?
+
+    # Need to combine installed with added modules.
+    local combinedPackageModules="$(mergeLists --field-separator , --sort --omit-empty -- "$packageModules" "${installedPipxPackages["$packageName"]}" "${addedPipxPackages["$packageName"]}" "${externallyAddedPipxPackages["$packageName"]}")"
+    local combinedPackageSpec="${packageName}${combinedPackageModules:+[${combinedPackageModules}]}"
+
+    preinstallHook "$combinedPackageSpec"
+    addedPipxPackages["$packageName"]="$combinedPackageModules"
+    postinstallHook "$combinedPackageSpec"
+}
+
+isAvailablePipx()
+{
+    isQuiet=t hasPipx "$@"
+}
+
+installPipx()
+{
+    [ ${#addedPipxPackages[@]} -gt 0 ] || return
+    local collectedQuotedPackageSpecs=
+    for packageName in "${!addedPipxPackages[@]}"
+    do
+	local packageModules="${addedPipxPackages["$packageName"]}"
+	local quotedPackageSpec; printf -v quotedPackageSpec %q "${packageName}${packageModules:+[${packageModules}]}"
+	collectedQuotedPackageSpecs+="${collectedQuotedPackageSpecs:+ }${quotedPackageSpec}"
+    done
+
+    local IFS=' '
+    submitInstallCommand "${SUDO}${SUDO:+ }pipx install --global --force $collectedQuotedPackageSpecs"
+    # Note: --force to update existing packages when modules have been (potentially) added.
+}
+
+typeRegistry+=([pipx:]=Pipx)
+typeInstallOrder+=([305]=Pipx)
