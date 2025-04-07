5cb743677f73daf9c77efe1880ff10f71e834baa
Ingo Karkat <swdev@ingo-karkat.de>
2025-04-07 19:15:18 +0200

    ENH: Add generic completion function for use by software-setups
    There's no need to define it here (yet), as the software project itself doesn't ship with any definitions.
    
    System-info: Ubuntu 24.04.2 LTS, x86_64
    Platform-info: bash 5.2.21(1)-release

---
+++ shell/bash/completions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash source-this-script
+
+_setup_software_complete()
+{
+    local softwareDefinitionsDirspec="${1:?}"; shift
+    local IFS=$'\n'
+    COMPREPLY=()
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+
+    readarray -t COMPREPLY < <(
+	cd "$softwareDefinitionsDirspec" \
+	    && readarray -t files < <(find . -type f -printf '%P\n') \
+	    && compgen -W "${files[*]}" -- "$cur"
+    )
+    [ ${#COMPREPLY[@]} -gt 0 ] && readarray -t COMPREPLY < <(printf "%q\n" "${COMPREPLY[@]}")
+}
+
+# Usage:
+#_setup_TODO_software_complete()
+#{
+#    _setup_software_complete /path/to/TODO-software/etc/definitions "$@"
+#}
+#complete -F _setup_TODO_software_complete setup-TODO-software
