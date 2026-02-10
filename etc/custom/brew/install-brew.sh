36648ea3cc48fcc62d3e2fcb0b83070b930af2ff
Ingo Karkat <swdev@ingo-karkat.de>
2026-02-10 19:38:59 +0100

    ENH: Add brew: type for Homebrew on Linux
    This is a package manager like pip3: or pipx:, but the installation is a bit more involved, as we're trying to prevent manipulation by the current user.
    Homebrew already installs under its /home/linuxbrew prefix, but by default allows modifications (install, update) by the current user. To emulate the behavior of pipx --global, I create a service account and change the access rights.
    
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: bash 5.0.17(1)-release

---
+++ etc/custom/brew/install-brew.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+: ${SUDO:=sudo}; [ $EUID -eq 0 ] && SUDO=''
+case ",${DEBUG:-}," in *,sudo,*) SUDO="verbose $SUDO";; *,sudo\!,*) SUDO="echotrace $SUDO";; esac
+
+: ${LINUXBREW_HOME:=/home/linuxbrew}
+: ${BASH_LOGIN_FILESPEC:=${LINUXBREW_HOME}/.bash_login}
+
+printUsage()
+{
+    cat <<HELPTEXT
+Install the Homebrew package manager under a service account so that regular
+users have to be a member of the linuxbrew group to use its packages and use
+sudo to change packages.
+HELPTEXT
+    echo
+    printf 'Usage: [DEBUG=sudo[!]] %q %s\n' "$(basename "$1")" '[--check] [-?|-h|--help]'
+}
+
+ECHO=echo
+typeset -a checkArg=()
+isCheck=
+case "$1" in
+	--help|-h|-\\?)	shift; printUsage "$0"; exit 0;;
+	--check)	checkArg=("$1"); shift; ECHO=:; isCheck=t; set -e;; # set -e aborts on the first failing check, but failures in the actual setup will continue with the next.
+	-*)		{ echo "ERROR: Unknown option \"$1\"!"; echo; printUsage "$0"; } >&2; exit 2;;
+esac
+if [ $# -ne 0 ]; then
+    printUsage "$0" >&2
+    exit 2
+fi
+
+# Linux system account; i.e. dedicated user and group.
+isNewBrewSystemAccount=
+if ! getent group linuxbrew >/dev/null 2>&1 \
+    || ! getent passwd linuxbrew >/dev/null 2>&1
+then
+    [ "$isCheck" ] && exit 1
+    $SUDO adduser --system --group --home "$LINUXBREW_HOME" --shell /bin/bash linuxbrew || exit $?
+    isNewBrewSystemAccount=t
+fi
+
+# Allow read access to the system account's home directory itself; all Homebrew
+# files are in the ./.linuxbrew/ subdir, anyway. This simplifies the following
+# checks for .bash_login.
+if ! hasFileAttributes --mode o+rx -- "$LINUXBREW_HOME"; then
+    [ "$isCheck" ] && exit 1
+    $SUDO chmod o+rx -- "$LINUXBREW_HOME"
+fi
+
+checkedAddOrUpdate "${checkArg[@]}" \
+	--sudo-command sudoWithUnixhome \
+	--no-subsequent-backup \
+	addOrUpdateLine \
+		--create-nonexisting \
+		--line 'umask 0027  # Group is read-only, others have no access.' \
+		-- "$BASH_LOGIN_FILESPEC"
+
+checkedAddOrUpdate "${checkArg[@]}" \
+	--sudo-command sudoWithUnixhome \
+	--no-subsequent-backup \
+	addOrUpdateLine \
+		--create-nonexisting \
+		--line "eval \"\$(${LINUXBREW_HOME}/.linuxbrew/bin/brew shellenv bash)\"" \
+		-- "$BASH_LOGIN_FILESPEC"
+
+[ "$isCheck" ] || $SUDO chown linuxbrew:linuxbrew -- "$BASH_LOGIN_FILESPEC"
+
+hasBrew()
+{
+    [ -x "${LINUXBREW_HOME}/.linuxbrew/bin/brew" ] && return 0
+    [ -d "${LINUXBREW_HOME}/.linuxbrew" ] || return 1	# Our user is able to read the user's home directory, but not the contents of the .linuxbrew tree.
+
+    # System account just got set up; can't be installed yet.
+    [ "$isNewBrewSystemAccount" ] && return 1
+
+    # If the current user in the current session is not in the linuxbrew group, they
+    # cannot check; need to use the linuxbrew user.
+    isBelongsToGroup linuxbrew && return 1  # Our user was able to check, didn't find it.
+    sudo --user linuxbrew bash -c "[ -x '${LINUXBREW_HOME}/.linuxbrew/bin/brew' ]"
+}
+
+if ! hasBrew; then
+    [ "$isCheck" ] && exit 1
+    sudo --user linuxbrew --set-home --login bash -c 'NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"' || exit $?
+fi
---
+++ lib/types/brew.sh
@@ -0,0 +1,87 @@
+#!/bin/bash source-this-script
+
+configUsageBrew()
+{
+    cat <<'HELPTEXT'
+brew: items refer to the Homebrew package manager.
+HELPTEXT
+}
+
+typeset -A installedBrewPackages=()
+isInstalledBrewPackagesAvailable=
+getInstalledBrewPackages()
+{
+    [ "$isInstalledBrewPackagesAvailable" ] && return
+
+    typeset -a brewLauncher=()
+    if exists brew; then
+	# If the brew command is provided, use it.
+	:
+    elif [ ! -d ~linuxbrew ]; then
+	# A missing brew home directory means that no Homebrew itself has been installed
+	# yet.
+	isInstalledBrewPackagesAvailable=t
+	return
+    elif sudo --user linuxbrew bash -c '[ -x /home/linuxbrew/.linuxbrew/bin/brew ]'; then
+	# Homebrew has already been installed, but our user doesn't (yet) have access to
+	# it. (A new login is required to apply the group membership in "linuxbrew", and
+	# the brew wrapper command is also only created on the next login.) Need to use
+	# the dedicated linuxbrew user to access it (so far).
+	brewLauncher=(sudo --user linuxbrew --set-home --login)
+    else
+	# A missing brew command (even though the home directory might already exist)
+	# means that Homebrew hasn't been fully installed yet.
+	isInstalledBrewPackagesAvailable=t
+	return
+    fi
+
+    local exitStatus packageName remainder; while IFS=' ' read -r packageName remainder || { exitStatus="$packageName"; break; }	# Exit status from the process substitution (<(brew)) is lost; return the actual exit status via an incomplete (i.e. missing the newline) last line.
+    do
+	case "$packageName" in
+	    '==> '*)	    continue;;	# Skip formulae / casks headers.
+	    '')		    continue;;	# Skip empty lines.
+	    *)		    installedBrewPackages["$packageName"]=t
+			    case ",${DEBUG:-}," in *,setup-software:brew,*) echo >&2 "${PS4}setup-software (brew): Found installed ${packageName}";; esac
+			    ;;
+	esac
+    done < <("${brewLauncher[@]}" brew list -1 --quiet 2>/dev/null; printf %d "$?")
+    [ $exitStatus -eq 0 ] && isInstalledBrewPackagesAvailable=t
+}
+
+typeset -A addedBrewPackages=()
+typeset -A externallyAddedBrewPackages=()
+hasBrew()
+{
+    local brewPackageName="${1:?}"; shift
+    if ! getInstalledBrewPackages; then
+	messagePrintf >&2 'ERROR: Failed to obtain installed Homebrew package list; skipping %s.\n' "$brewPackageName"
+	return 99
+    fi
+
+    [ "${installedBrewPackages["$brewPackageName"]}" ] || [ "${addedBrewPackages["$brewPackageName"]}" ] || [ "${externallyAddedBrewPackages["$brewPackageName"]}" ]
+}
+
+addBrew()
+{
+    local brewPackageName="${1:?}"; shift
+    isAvailableOrUserAcceptsGroup brew "${projectDir}/lib/definitions/brew" 'Homebrew package manager' || return $?
+
+    preinstallHook Brew "$brewPackageName"
+    addedBrewPackages["$brewPackageName"]=t
+    postinstallHook Brew "$brewPackageName"
+}
+
+isAvailableBrew()
+{
+    isQuiet=t hasBrew "$@"
+}
+
+installBrew()
+{
+    [ ${#addedBrewPackages[@]} -gt 0 ] || return
+    local IFS=' '
+    submitInstallCommand "sudo --user linuxbrew --set-home --login ${isBatch:+ NONINTERACTIVE=1 }brew install ${!addedBrewPackages[*]}"
+}
+
+typeRegistry+=([brew:]=Brew)
+typeInstallOrder+=([600]=Brew)
